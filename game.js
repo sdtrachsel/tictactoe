b9b79d2d49d7a4a1f5b245d96b4ff0c9d461f02c
Sharie Trachsel <strachsel22@turing.edu>
2023-02-28 19:45:02 -0600

    Add selectors and assets

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 class Game {
-	constructor() {
+	constructor(playerOne, playerTwo) {
 //** Array of 2 objects *****
-		this.players = [];
+		this.players = [playerOne, playerTwo];
 
 // ** Array of 9 with player objects or just icon? ****
 // top  		left: 0 center: 1, right: 2 
-// middle		left: 0 center: 1, right: 2 
-// bottom		left: 0 center: 1, right: 2 
+// middle		left: 3 center: 4, right: 5 
+// bottom		left: 6 center: 7, right: 8 
 		this.currentGameBoard = [];
 
 // ***** may not need? Possible toggle? *****
--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 // Query Selectors
+var gameStatusHeader = document.getElementById('gamestatus')
+var playerOneWins = document.getElementById('pOneWins')
+var playerTwoWins = document.getElementById('pTwoWins')
+
+var topLeft = document.getElementById('tcSpace')
+var topCenter = document.getElementById('tcSpace')
+var topRight = document.getElementById('trSpace')
+
+var middleLeft = document.getElementById('mlSpace')
+var middleCenter = document.getElementById('mcSpace')
+var middleRight = document.getElementById('mrSpace')
+
+var bottomLeft = document.getElementById('blSpace')
+var bottomCenter = document.getElementById('bcSpace')
+var bottomRight = document.getElementById('brSpace')
 
 // Eventlisteners
+/// upon load create 2 players and start a new game
 
 // Variables
 

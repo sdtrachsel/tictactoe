564fe69199ebc8b8dac27790136ff8f90d9f37bc
Sharie Trachsel <strachsel22@turing.edu>
2023-03-01 19:21:11 -0600

    Add player win count to dom and game is a draw display

--- main.js
+++ main.js
@@ -11,44 +11,45 @@ var gameBoard = document.getElementById('gameBoard')
 gameBoard.addEventListener('click', placeToken)
 
 // Variables
-var gameRound = new Game(new Player("one", "./assets/badger.png","player one icon badger"), new Player("two", "./assets/blowfish.png", "player two icon blowfish"))
+var gameRound = new Game(new Player("one", "./assets/badger.png", "player one icon badger"), new Player("two", "./assets/blowfish.png", "player two icon blowfish"))
 
 
 // Event Handlers
-
-
 function placeToken() {
 	if (event.target.classList.contains("board-space")) {
-// Place token
-	//data model		
+		// Place token
+
 		var selectedBoardSpace = event.target.id
 		gameRound.gameBoard[Number(selectedBoardSpace)] = gameRound.currentPlayer
 
-	//dom 
 		var tokenElement = document.createElement('img')
 		tokenElement.src = gameRound.currentPlayer.token
 		tokenElement.alt = gameRound.currentPlayer.altText
 		event.target.appendChild(tokenElement)
 
 // Check for winner 
-	// data model
-		if(gameRound.checkWin()){
-			if(gameRound.currentPlayer.id.includes('one')){
+		if (gameRound.checkWin()) {
+			if (gameRound.currentPlayer.id.includes('one')) {
 				gameRound.playerOne.wins++;
-
+				playerOneWins.innerText =`${gameRound.playerOne.wins} Wins`
 				gameStatusHeader.innerHTML = `
 				<img class="winner-icon" src=${gameRound.playerOne.token} alt=${gameRound.playerOne.altText}> won!
 				`
-				} else {
+			} else if (gameRound.currentPlayer.id.includes('two')) {
 				gameRound.playerTwo.wins++
+				playerOneWins.innerText =`${gameRound.playerTwo.wins} Wins`
+				gameStatusHeader.innerHTML = `
+				<img class="winner-icon" src=${gameRound.playerTwo.token} alt=${gameRound.playerTwo.altText}> won!
+				`
 			}
+		} else if (gameRound.gameBoard.length === 9 && !gameRound.gameBoard.includes(undefined)) {
+			gameStatusHeader.innerHTML = "It's a draw";			
 		} else {
-	//Change Turn 
-	//DataModel
-		gameRound.changeTurn()
-	//DOM
-		playerTurnIcon.src = gameRound.currentPlayer.token
+//Change Turn 
+			gameRound.changeTurn()
+			playerTurnIcon.src = gameRound.currentPlayer.token
 		}
+
 
 	}
 }
